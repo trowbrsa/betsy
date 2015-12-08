1632b6a65c133325dfda67b88092df78fab38b40
Jennie Buechner <janina.buechner@gmail.com>
2015-12-08 14:21:38 -0800

    add default true to active field in product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208201429) do
+ActiveRecord::Schema.define(version: 20151208221927) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -48,9 +48,9 @@ ActiveRecord::Schema.define(version: 20151208201429) do
     t.string   "photo_url"
     t.integer  "stock"
     t.string   "description"
-    t.boolean  "active"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.boolean  "active",      default: true
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "reviews", force: :cascade do |t|
