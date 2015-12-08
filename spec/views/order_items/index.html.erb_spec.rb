6d04ac9fc464458776fded804a04a5b28fe8c286
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-08 12:10:31 -0800

    Add OrderItem model

---
+++ spec/views/order_items/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "order_items/index", type: :view do
+  before(:each) do
+    assign(:order_items, [
+      OrderItem.create!(
+        :product_id => 1,
+        :order_id => 2,
+        :quantity => 3
+      ),
+      OrderItem.create!(
+        :product_id => 1,
+        :order_id => 2,
+        :quantity => 3
+      )
+    ])
+  end
+
+  it "renders a list of order_items" do
+    render
+    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+    assert_select "tr>td", :text => 3.to_s, :count => 2
+  end
+end
---
+++ spec/views/order_items/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "order_items/show", type: :view do
+  before(:each) do
+    @order_item = assign(:order_item, OrderItem.create!(
+      :product_id => 1,
+      :order_id => 2,
+      :quantity => 3
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/1/)
+    expect(rendered).to match(/2/)
+    expect(rendered).to match(/3/)
+  end
+end
