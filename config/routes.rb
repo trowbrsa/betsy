d50b18050ac543c1e64b3289777567145083623b
Jennie Buechner <janina.buechner@gmail.com>
2015-12-08 12:19:48 -0800

    create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :order_items
   root 'welcome/index'
+  resources :order_items
   resources :orders
   resources :products
   resources :categories
   resources :users
+
+  # get 'login/' => 'sessions#new', as: :login
+  # post 'login/' => 'sessions#create'
+  # delete 'logout/' => 'sessions#destroy', as: :logout
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
