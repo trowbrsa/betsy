312c06e6ee647dba343d050bac89f6e72525e836
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-08 14:04:43 -0800

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :products
   has_secure_password
+
+  validates :username, :email, presence: true
+  validates :username, :email, uniqueness: true
+  # Password Confirmation must match Password
 end
