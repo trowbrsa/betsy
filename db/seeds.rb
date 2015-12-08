fa03a086253d15823ef1a27cfa822e3aa32530ea
Jennie Buechner <janina.buechner@gmail.com>
2015-12-08 14:57:17 -0800

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+tables = {
+  "Order" => './seeds_csvs/orders.csv',
+  "Product" => './seeds_csvs/products.csv',
+  "User" => './seeds_csvs/users.csv',
+  "Review" => './seeds_csvs/reviews.csv',
+  'OrderItem' => './seeds_csvs/order_items.csv'
+  }
+
+  tables.each do |k, v|
+    data = CSV.read(v, :headers => true, :header_converters => :symbol).map{ |row| row.to_hash }
+    data.each do |info|
+      if k == "Order"
+        Order.create(info)
+      elsif k == "Product"
+        Product.create(info)
+      elsif k == "User"
+        User.create(info)
+      elsif k == "Review"
+        Review.create(info)
+      elsif k == "OrderItem"
+        OrderItem.create(info)
+      end
+    end
+  end
