996240db467182360126d623a164d1543b87e3d9
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-08 14:38:14 -0800

    add OrderItem model validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :order
+
+  validates :quantity, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+
 end
