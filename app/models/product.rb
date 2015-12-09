c4063408145266eaaa2b5b22d92698e5e91524e9
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-09 09:02:23 -0800

    Add back product name uniqueness

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
 
   validates :name, :price, presence: true
+  validates :name, uniqueness: true
   validates_numericality_of :price, :greater_than => 0
   validates :photo_url, format: {with: /\.(png|jpg)\Z/i}, allow_nil: true
 
