7b00a21837b4718322a124f1144c0769a1d6de3a
Jennie Buechner <janina.buechner@gmail.com>
2015-12-09 08:48:30 -0800

    remove name unique from product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,6 @@ class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
 
   validates :name, :price, presence: true
-  validates :name, uniqueness: true
   validates_numericality_of :price, :greater_than => 0
   validates :photo_url, format: {with: /\.(png|jpg)\Z/i}, allow_nil: true
 
