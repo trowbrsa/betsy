5c3d9f62e13603a4e48e76380e10e259d7275606
Jennie Buechner <janina.buechner@gmail.com>
2015-12-10 09:36:18 -0800

    add user validation for lowercase email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :products
   has_secure_password
 
+  before_save { self.email = email.downcase } # ensures emails are all lowercase
+
   validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
