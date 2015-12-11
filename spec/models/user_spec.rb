8fce019ff4ca7d3ffd99bef4cbeed58b6b12ba99
Tammy <misstamar@gmail.com>
2015-12-10 16:26:10 -0800

    almost done with user model RSPEC

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
-# require 'rails_helper'
-#
-# RSpec.describe User, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe ".validates" do
+    let(:user1) do
+      User.create(name: "Nemo", username: "Nemo1", email: "nemo@gmail.com", password: "123", password_confirmation: "123")
+    end
+    let(:user2) do
+      User.create(name: "Nemo2", username: "Nemo2", email: "nemo@gmail.com", password: "123", password_confirmation: "123")
+    end
+    it "must have a username" do
+      expect(User.new(username: nil)).to_not be_valid
+    end
+
+    it "must have an email" do
+      expect(User.new(email: nil)).to_not be_valid
+    end
+
+    it "should have uniqueness email" do
+      expect(:user2).to_not be_vaild
+    end
+  end
+
+
+end
