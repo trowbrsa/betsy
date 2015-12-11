0d83687043440172cb8e2d5f3fed9048eef3ed04
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-10 16:33:11 -0800

    Complete product#create and product#update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
-  before_action :find_product, only: [:show, :edit, :update, :destroy]
+  before_action :find_product, only: [:show, :edit, :update]
   before_action :find_user, only: [:new, :edit, :create, :update]
+  before_action :all_categories, only: [:new, :edit, :create, :update]
 
   def index
     @products = Product.all.paginate(page: params[:page], per_page: 12)
@@ -13,19 +14,19 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-    @categories = Category.all
   end
 
   def edit
-    @categories = Category.all
-    @product_categories = @product.categories
   end
 
   def create
-    @categories = Category.all
     @product = Product.create(product_params) do |p|
       p.user_id = @user.id
-      p.categories = Category.find(params[:categories])
+      if params[:categories].nil?
+        p.categories = []
+      else
+        p.categories = Category.find(params[:categories])
+      end
     end
     if @product.save
       redirect_to user_products_path(@product.user)
@@ -35,9 +36,12 @@ class ProductsController < ApplicationController
   end
 
   def update
-    @product.update(product_params)
-    @product.categories = Category.find(params[:categories])
-    @product.save
+    @product.assign_attributes(product_params)
+    if params[:categories].nil?
+      @product.categories = []
+    else
+      @product.categories = Category.find(params[:categories])
+    end
     if @product.save
       redirect_to user_products_path(@product.user)
     else
@@ -47,15 +51,19 @@ class ProductsController < ApplicationController
 
   private
 
-    def find_product
-      @product = Product.find(params[:id])
-    end
+  def find_product
+    @product = Product.find(params[:id])
+  end
 
-    def find_user
-      @user = User.find(params[:user_id])
-    end
+  def find_user
+    @user = User.find(params[:user_id])
+  end
 
-    def product_params
-      params.require(:product).permit(:name, :price, :photo_url, :stock, :description, :active)
-    end
+  def all_categories
+    @categories = Category.all
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :photo_url, :stock, :description, :active)
+  end
 end
