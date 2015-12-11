4b5132cb54e089e9a1aeb8e144df3e68a5915e8c
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-11 15:59:16 -0800

    Complete tests in model/order_spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,34 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  # let (:sample_order) {
-  #   Order.create(zip: "02780")
-  # }
-
   describe ".validates" do
     it { is_expected.to validate_numericality_of(:zip) }
     it { is_expected.to validate_numericality_of(:cc_num) }
-    
+    it { is_expected.to validate_length_of(:zip) }
+
+    it "must have a nil or valid email " do
+      expect(Order.create(email: 'user@foo,com')).to_not be_valid
+      expect(Order.create(email: 'user_at_foo.org')).to_not be_valid
+      expect(Order.create(email: 'example.user@foo.')).to_not be_valid
+      expect(Order.create(email: 'foo@bar_baz.com')).to_not be_valid
+      expect(Order.create(email: 'foo@bar+baz.com')).to_not be_valid
+      expect(Order.create(email: ' ')).to_not be_valid
+      expect(Order.create(email: 'kelly@kelly.com')).to be_valid
+      expect(Order.create(email: nil)).to be_valid
+    end
   end
 end
-
-# class Order < ActiveRecord::Base
-#   has_many :order_items
-#   validates_numericality_of :zip, :cc_num, allow_nil: true
-#   validates_length_of :zip, is: 5, allow_nil: true
-#   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, allow_nil: true
-# end
-
-# create_table "orders", force: :cascade do |t|
-#   t.string   "email"
-#   t.string   "street"
-#   t.string   "city"
-#   t.string   "state"
-#   t.string   "zip"
-#   t.string   "cc_num"
-#   t.date     "cc_exp"
-#   t.integer  "cc_cvv"
-#   t.string   "cc_name"
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-# end
