1908e067f42aae03593eb73a6e305237021d1d60
Tammy <misstamar@gmail.com>
2015-12-13 19:53:01 -0800

    finisg user model RSPEC

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,38 +2,23 @@ require 'rails_helper'
 
 
 RSpec.describe User, type: :model do
-  describe ".validates" do
-    let(:user1) do
+
+    let(:user1) {
       User.create(name: "Nemo", username: "Nemo1", email: "nemo@gmail.com", password: "123", password_confirmation: "123")
-    end
-    let(:user2) do
-      User.create(name: "Nemo2", username: "Nemo2", email: "nemo@gmail.com", password: "123", password_confirmation: "123")
-    end
-    it "must have a username" do
-      expect(User.new(username: nil)).to_not be_valid
+    }
+
+  describe ".validates" do
+    it { is_expected.to validate_presence_of(:email) }
+
+    it "must have a unique email" do
+      User.create(name: "Bob", username: "bobi", email: "nemo@gmail.com", password: "333", password_confirmation: "333")
+      expect(User.new(name: "Nemo", username: "Nemo1", email: "nemo@gmail.com", password: "123", password_confirmation: "123")).to_not be_valid
     end
 
-    it "must have an email" do
-      expect(User.new(email: nil)).to_not be_valid
-    end
-  end
-  #
-  describe "uniqueness" do
-    it do
-      should validate_uniqueness_of(:email)
+    it "must have an email format" do
+      expect(User.new(name: "Nemo", username: "Nemo1", email: "nemo.gmail.com", password: "123", password_confirmation: "123")).to_not be_valid
     end
   end
 
-  # it "should have uniqueness email" do
-  #   expect validate_uniqueness_of(:email)
-  # end
-  #   it "should have uniqueness email" do
-  #     expect(:user2).to be_vaild
-  #   end
-  # end
-  #
-  #
-  # it { validate_uniqueness_of(:email) }
-  # end
 
 end
