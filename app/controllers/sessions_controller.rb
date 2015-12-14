37b3ec606af0c6f9da12ceb57eb28c21893a2b34
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-14 09:47:00 -0800

    Add sessions#create and sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,24 @@ class SessionsController < ApplicationController
   end
 
   def create
-    raise
+    data = params[:session_data]
+    @user = User.find_by_username(data[:username])
+    if !@user.nil?
+      if @user.authenticate(data[:password])
+        session[:user_id] = @user.id
+        redirect_to root_path
+      else
+        flash.now[:error] = "Try again."
+        render :new
+      end
+    else
+      flash[:error] = "Register New Account"
+      redirect_to new_user_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
-
 end
