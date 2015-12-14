a2dc8e9eff3aa54714a61bdb19c7fa2e7985cb21
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-14 10:13:28 -0800

    add has_many through relationship between products and orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :order_items
+  has_many :products, through: :order_items
   validates_numericality_of :zip, :cc_num, allow_nil: true
   validates_length_of :zip, is: 5, allow_nil: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, allow_nil: true
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :user
-  has_many :orders
+  has_many :order_items
+  has_many :orders, through: :order_items
   has_many :reviews
   has_and_belongs_to_many :categories
 
