f6bbc0556fe8a91ad325b78b18fd2fe6671e5b85
Jennie Buechner <janina.buechner@gmail.com>
2015-12-14 15:29:05 -0800

    update user model and specs to downcase email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151214193805) do
+ActiveRecord::Schema.define(version: 20151214214750) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,28 @@
 require 'rails_helper'
 
-
 RSpec.describe User, type: :model do
-
-    let(:user1) {
-      User.create(name: "Nemo", username: "Nemo1", email: "nemo@gmail.com", password: "123", password_confirmation: "123")
-    }
+  let (:user) { FactoryGirl.create(:user) }
+  it { is_expected.to have_secure_password }
 
   describe ".validates" do
     it { is_expected.to validate_presence_of(:email) }
 
     it "must have a unique email" do
-      User.create(name: "Bob", username: "bobi", email: "nemo@gmail.com", password: "333", password_confirmation: "333")
-      expect(User.new(name: "Nemo", username: "Nemo1", email: "nemo@gmail.com", password: "123", password_confirmation: "123")).to_not be_valid
+      expect(User.new(name: "Nemo", username: "Nemo1", email: user.email, password: "123", password_confirmation: "123")).to_not be_valid
     end
 
-    it "must have a valid email " do
-      expect(User.create(email: 'nemo@foo,com', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
-      expect(User.create(email: 'nemo_at_foo.org', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
-      expect(User.create(email: 'nemo.user@foo.', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
-      expect(User.create(email: 'nemo@bar_baz.com', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
-      expect(User.create(email: 'nemo@bar+baz.com', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
-      expect(User.create(email: ' ', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
-      expect(User.create(email: 'nemo@nemo.com', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to be_valid
+    it "downcases email before checking validity" do
+      expect(User.new(name: "Nemo", username: "Nemo1", email: user.email.upcase, password: "123", password_confirmation: "123")).to_not be_valid
+    end
+
+    it "must have a valid email" do
+      expect(User.new(email: 'nemo@foo,com', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
+      expect(User.new(email: 'nemo_at_foo.org', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
+      expect(User.new(email: 'nemo.user@foo.', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
+      expect(User.new(email: 'nemo@bar_baz.com', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
+      expect(User.new(email: 'nemo@bar+baz.com', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
+      expect(User.new(email: ' ', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to_not be_valid
+      expect(User.new(email: 'nemo@nemo.com', name: "Bob", username: "bobi", password: "333", password_confirmation: "333")).to be_valid
     end
 
   end
