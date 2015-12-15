9366ce1d539a2a16c0d12264450475dd519a582a
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-15 11:04:16 -0800

    Update orders show view and add presence validations to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
   has_many :order_items
   has_many :products, through: :order_items
+  validates :email, :street, :city, :state, :zip, :cc_num, :cc_exp, :cc_cvv, :cc_name, presence: true
   validates_numericality_of :zip, :cc_num, allow_nil: true
   validates_length_of :zip, is: 5, allow_nil: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, allow_nil: true
