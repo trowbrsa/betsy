6549eef19e86d96c618a7f2a74371d785eb98734
Kelly <kelly_devlin@alumni.brown.edu>
2015-12-14 17:13:32 -0800

    Update order controller specs

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
+  let (:sample_user) {
+    User.create(username: "Kelly", email: "Kelly@kelly.com", password: "password")
+  }
+
+  let (:sample_product) {
+    Product.create(name: "Rubber ducky",  price: "500", stock: "1", user_id: sample_user.id)
+  }
+
+  let (:sample_order_item) {
+    User.create(product_id: sample_product.id, order_id: sample_order.id, quantity: "3")
+  }
+
   let (:sample_order) {
     Order.create
   }
@@ -13,28 +25,31 @@ RSpec.describe OrdersController, type: :controller do
 
   describe "GET 'index'" do
     it "is successful" do
-      get :index
+      session[:user_id] = sample_user.id
+      get :index, user_id: sample_user.id
       expect(response.status).to eq 200
     end
   end
 
   describe "GET 'show'" do
     it "renders the show view" do
-      get :show, id: sample_order.id
+      get :show, id: sample_order.id, user_id: sample_user.id
       expect(subject).to render_template :show
     end
   end
 
   describe "GET 'new'" do
     it "renders new view" do
-      get :new
+      session[:cart] = { 1 => 2, 3 => 4 }
+      get :new, user_id: sample_user.id
       expect(subject).to render_template :new
     end
   end
 
   describe "POST 'create'" do
     it "redirects to index page" do
-      post :create
+      session[:cart] = { 1 => 2, 3 => 4 }
+      post :create, { user_id: sample_user.id } 
       expect(subject).to redirect_to orders_path
     end
 
@@ -44,31 +59,31 @@ RSpec.describe OrdersController, type: :controller do
     end
   end
 
-  describe "GET 'edit'" do
-    it "renders edit view" do
-      get :edit, id: sample_order.id
-      expect(subject).to render_template :edit
-    end
-  end
+  # describe "GET 'edit'" do
+  #   it "renders edit view" do
+  #     get :edit, id: sample_order.id
+  #     expect(subject).to render_template :edit
+  #   end
+  # end
 
-  describe "PATCH 'update'" do
-    it "redirects to index page" do
-      patch :update, { order: { zip: "02780" } }
-      expect(subject).to redirect_to orders_path
-      expect(Order.all.last.zip).to eq "02780"
-    end
+  # describe "PATCH 'update'" do
+  #   it "redirects to index page" do
+  #     patch :update, { order: { zip: "02780" } }
+  #     expect(subject).to redirect_to orders_path
+  #     expect(Order.all.last.zip).to eq "02780"
+  #   end
+  #
+  #   it "renders edit template on error" do
+  #     patch :update, bad_params
+  #     expect(subject).to render_template :edit
+  #     expect(Order.all.last.zip).to eq nil
+  #   end
+  # end
 
-    it "renders edit template on error" do
-      patch :update, bad_params
-      expect(subject).to render_template :edit
-      expect(Order.all.last.zip).to eq nil
-    end
-  end
-
-  describe "DELETE 'destroy'" do
-    it "redirects to index page" do
-      delete :destroy, id: sample_order.id
-      expect(subject).to redirect_to orders_path
-    end
-  end
+  # describe "DELETE 'destroy'" do
+  #   it "redirects to index page" do
+  #     delete :destroy, id: sample_order.id
+  #     expect(subject).to redirect_to orders_path
+  #   end
+  # end
 end
