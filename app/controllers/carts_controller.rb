dd48d34f60f59aa6a8208e4d2fc15ddc4f394d1b
Tammy <misstamar@gmail.com>
2015-12-15 14:47:44 -0800

    still canot update quantity in the cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -25,7 +25,7 @@ class CartsController < ApplicationController
         cart[product_id] = cart[product_id] + 1
         redirect_to cart_path
       else
-        flash[:error] = "The product is not on stock"
+        flash[:error] = "The product is out of stock"
         redirect_to user_product_path(product.user_id, product.id)
       end
     else
