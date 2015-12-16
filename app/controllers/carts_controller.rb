2f31f5daf6d7ca4fc7744dbfa089355637deeda6
Jennie Buechner <janina.buechner@gmail.com>
2015-12-16 09:53:42 -0800

    refactor carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,28 +7,18 @@ class CartsController < ApplicationController
   def add
     product = Product.find(params[:id])
     product_id = params[:id]
-    if session[:cart]
-      cart = session[:cart]
-    else
-      session[:cart] = {}
-      cart = session[:cart]
-    end
-    # if the cart already has the product -> just add one
-    # if not -> set the quantity to one
-    if cart[product_id]
-      #check that the product is on stock
-      if product.stock >= cart[product_id] + 1
-        cart[product_id] = cart[product_id] + 1
-        redirect_to cart_path
-      else
-        flash[:error] = "The product is out of stock"
-        redirect_to user_product_path(product.user_id, product.id)
-      end
-    else
+    session[:cart] = {} if !session[:cart]
+    cart = session[:cart]
+    if cart[product_id].nil? && product.stock > 0 # adding product for the first time
       cart[product_id] = 1
       redirect_to cart_path
+    elsif cart[product_id] && product.stock >= cart[product_id] + 1 # adding product already in cart
+      cart[product_id] += 1
+      redirect_to cart_path
+    else
+      flash[:error] = "Sorry, there is not enough product in stock to fulfill your request."
+      redirect_to user_product_path(product.user_id, product.id)
     end
-
   end
 
   def clearCart
