b1251d3c4b18bc600b82f4d7a7148d89b0a52000
Jennie Buechner <janina.buechner@gmail.com>
2016-01-17 16:30:40 -0800

    delete commented out code

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,40 +1,10 @@
 class OrderItemsController < ApplicationController
-  # before_action :set_order_item, only: [:show, :edit, :update, :destroy]
-  #
   def index
     @user = User.find(params[:user_id])
     @orders = @user.orders
   end
-  #
-  # def show
-  #   @user = User.find(params[:user_id])
-  # end
-  #
-  # def new
-  #   @order_item = OrderItem.new
-  #   @order = Order.find(params[:order_id])
-  #   @user = User.find(params[:user_id])
-  # end
-  #
-  # def edit
-  #   @user = User.find(params[:user_id])
-  #   @order = Order.find(params[:order_id])
-  #   @user = User.find(params[:user_id])
-  # end
-  #
-  # def create
-  #   @user = User.find(params[:user_id])
-  #   @order = Order.find(params[:order_id])
-  #   @order_item = OrderItem.new(order_item_params)
-  #     if @order_item.save
-  #       redirect_to user_order_order_items_path(@user,@order.id)
-  #     else
-  #       render "new"
-  #     end
-  # end
 
   def shipped
-    # user = User.find(params[:user_id])
     order_item = OrderItem.find(params[:id])
     if !order_item.shipped
       order_item.update(:shipped => true)
@@ -45,26 +15,4 @@ class OrderItemsController < ApplicationController
     end
     redirect_to :back
   end
-
-  # def update
-  #  @order_item.update(order_item_params)
-  #  redirect_to user_order_order_items_path
-  # end
-  #
-  # def destroy
-  #   @user = User.find(params[:user_id])
-  #   @order = Order.find(params[:order_id])
-  #   @order_item.destroy
-  #   redirect_to user_order_order_items_path(@user, @order)
-  # end
-  #
-  # private
-  #
-  #   def set_order_item
-  #     @order_item = OrderItem.find(params[:id])
-  #   end
-
-    # def order_item_params
-    #   params.require(:order_item).permit(:product_id, :order_id, :quantity)
-    # end
 end
